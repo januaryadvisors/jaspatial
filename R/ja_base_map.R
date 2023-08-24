c43ab6dcd59e21f494c51637b2b9a16cd2dbb760
davidmcclendon <davidmmcclendon@gmail.com>
2023-08-23 19:22:18 -0500

    add minZoom, maxZoon, dragging, and tap to options

--- R/ja_base_map.R
+++ R/ja_base_map.R
@@ -8,31 +8,53 @@
 #' @param .lon Longitude for setting center of map
 #' @param .lat Latitude for setting center of map
 #' @param .zoom_level Zoom level for setting map
+#' @param .minZoom Set minimum Zoom level for map
+#' @param .maxZoom Set max zoom level for map
+#' @param .dragging turn dragging ability on/off; defaults False
+#' @param .tap turn tap ability on/off; default False
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #' ja_base_map()
-ja_base_map <- function(.lon = -95.3103, .lat = 29.7752, .zoom_level = 9) {
+ja_base_map <- function(.lon = -95.3103, .lat = 29.7752, .zoom_level = 9,
+                        .minZoom = 0, .maxZoom=18, .dragging = F, .tap=F) {
 
-  leaflet::leaflet() %>%
-    leaflet::addTiles(attribution = htmltools::HTML("Built by <a href='https://www.januaryadvisors.com/' target='_blank'>January Advisors</a>")) %>%
+  leaflet::leaflet(
+    minZoom = .minZoom,
+    maxZoom = .maxZoom,
+    dragging = .dragging,
+    tap = .tap
+  ) %>%
+    leaflet::addTiles(
+      attribution = htmltools::HTML(
+        "Built by <a href='https://www.januaryadvisors.com/' target='_blank'>January Advisors</a>"
+      )
+    ) %>%
     leaflet::addMapPane(name = "polygons", zIndex = 410) %>%
     leaflet::addMapPane(name = "maplabels", zIndex = 420) %>%
     leaflet::addProviderTiles("CartoDB.PositronNoLabels") %>%
-    leaflet::addProviderTiles("CartoDB.PositronOnlyLabels",
-                     options = leaflet::leafletOptions(pane = "maplabels"),
-                     group = "map labels") %>%
+    leaflet::addProviderTiles(
+      "CartoDB.PositronOnlyLabels",
+      options = leaflet::leafletOptions(pane = "maplabels"),
+      group = "map labels"
+    ) %>%
     leaflet::setView(.lon, .lat, zoom = .zoom_level) %>%
-    leaflet.extras2::addEasyprint(options = leaflet.extras2::easyprintOptions(
-      title = 'Print map',
-      position = 'topleft',
-      exportOnly = TRUE)) %>%
+    leaflet.extras2::addEasyprint(
+      options = leaflet.extras2::easyprintOptions(
+        title = 'Print map',
+        position = 'topleft',
+        exportOnly = TRUE
+      )
+    ) %>%
     leaflet.extras::addSearchOSM(
-      options = leaflet.extras::searchFeaturesOptions(zoom = 10, openPopup = TRUE,
-                                      position = "topright",
-                                      hideMarkerOnCollapse = TRUE)
+      options = leaflet.extras::searchFeaturesOptions(
+        zoom = 10,
+        openPopup = TRUE,
+        position = "topright",
+        hideMarkerOnCollapse = TRUE
+      )
     )
 
 }
